bd45cd5ef98f02cfbf0847e79203bd9b89450f3f
Thiago Rod <whytigo@gmail.com>
2022-03-03 11:03:17 -0500

    Created the questions and the generator file to handle Inquirer promts and rendering of the README

---
+++ generator.js
@@ -0,0 +1,59 @@
+function createReadMe(answers) {
+    return `
+
+# ${answers.projectName} 🏆 
+
+## Table of Contents ![badge](https://img.shields.io/badge/license-${answers.licensing}-blue)
+
+- [Description](#description)
+- [Installation](#installation)
+- [Toolkit](#toolkit)
+- [Licensing](#licensing)
+- [Usage](#usage)
+- [Licensing](#tests)
+- [Contributing](#contributing)
+- [Questions](#questions)
+
+### Description 💡 <a name="description"></a>
+
+${answers.description} 
+
+### Installation 💾 <a name="installation"></a>
+
+Here's how you setup this project:<br />
+
+${answers.installation}  
+
+### Toolkit 🧰 <a name="toolkit"></a>
+
+${answers.composition}  
+
+### Licensing ✨ <a name="licensing"></a> 
+
+This application is covered by the ${answers.licensing} license. 
+
+### Usage 💥 <a name="usage"></a> 
+
+${answers.usage} 
+
+### Tests 💎  <a name="tests"></a> 
+
+${answers.tests} 
+
+### Contributing 🤝 <a name="contributing"></a> 
+
+Resources and people who contributed include: ${answers.credits} 
+<br />
+
+If you would like to contribute to the project further, then: ${answers.contributing} 
+
+### Questions 😊 <a name="questions"></a> 
+
+Get in touch with me on Github: [${answers.username}](https://github.com/${answers.username})
+<br />
+
+If you have any additional questions, please contact me at: ${answers.email}
+`;
+}
+
+module.exports = createReadMe;
--- index.js
+++ index.js
@@ -1,2 +1,98 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const util = require("util");
+const generator = require("./generator")
+const writeAsync = util.promisify(fs.writeFile);
+
+function askUser() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectName',
+            message: 'What is your project named?',
+            default: 'The best project ever, of course!',
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your github handle?',
+            default: 'github name',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+            default: 'myname@domain.com',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What are the rules about using this project code, materials, etc.?',
+            default: 'People can use this for anything as long as it is not commercial.',
+        },
+
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What kind of testing was completed?',
+            default: 'I did x, y and z with testing.',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'How would you describe this project?',
+        },
+        {   // select from list
+            type: 'list',
+            name: 'licensing',
+            message: 'What License is this used with?',
+            choices: [
+                'Apache',
+                'Artistic',
+                'CC',
+                'Educational',
+                'MIT',
+                'Mozilla',
+                'Open',
+                'Other']
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'What should people do if they want to contribute to your project?',
+            default: 'Reach out on Github or via email with your plan.',
+        },
+        {   // check multiple options
+            type: 'checkbox',
+            name: 'composition',
+            message: 'What language(s) did you use to create your project?',
+            choices: [
+                'HTML', 'CSS', 'JavaScript', 'Ruby', 'React', 'Python', 'C', 'C#', 'TypeScript', 'Other'
+            ],
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide instructions to get started with your project.',
+        },
+
+        {
+            type: 'input',
+            name: 'credits',
+            message: 'Who, what, and how were you assisted in this project?',
+        }
+    ]);
+}
+async function init() {
+    try {
+        const answers = await askUser();
+        const buildContent = generator(answers);
+        writeAsync('./output/README.md', buildContent);
+        // overwrites last version - no new document
+        console.log('README.md successfully created in the written folder.');
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+init();
